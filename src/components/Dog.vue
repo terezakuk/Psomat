7c49196dfd8d5c1490b9e6cda7a884ee8315a615
Tereza Kukosova <tereza.kukosova@gmail.com>
2019-12-06 17:33:11 +0100

    Add Dog.vue, Dog.vue and Dogs.json

---
+++ src/components/Dog.vue
@@ -0,0 +1,25 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    props: {
+    cislo: {
+      type: String,
+      required: true
+    }
+  },
+  data: function() {
+    return {
+      loading: true,
+      
+    };
+  },
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Dogs.vue
@@ -0,0 +1,30 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    data: function() {
+    return {
+      loading: true,
+      psi: []
+    };
+  },
+  mounted() {
+    fetch("/api/dogs.json")
+      .then(response => response.json())
+      .then(data => {
+        this.psi = data.results;
+        this.loading = false;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+}
+</script>
+
+<style>
+
+</style>
