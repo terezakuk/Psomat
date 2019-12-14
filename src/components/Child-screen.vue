9bd3f9e7a157189ac84e1856f382de74aa3a6503
Tomáš Bončo <tomas.bonco@avast.com>
2019-12-14 22:36:29 +0100

    Uprava dog.vue

--- src/components/Child-screen.vue
+++ src/components/Child-screen.vue
@@ -1,18 +1,19 @@
 <template>
-  <div>
+	<div class="child-screen">
 
-    <router-link to="/search" class="tlacitko">
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon">
-			<path d="M7 13.1l8.9 8.9c.8-.8.8-2 0-2.8l-6.1-6.1 6-6.1c.8-.8.8-2 0-2.8L7 13.1z"/>
-		</svg>
-		Zpět
-	</router-link>
+		<router-link to="/search" class="tlacitko">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon">
+				<path d="M7 13.1l8.9 8.9c.8-.8.8-2 0-2.8l-6.1-6.1 6-6.1c.8-.8.8-2 0-2.8L7 13.1z"/>
+			</svg>
+			Zpět
+		</router-link>
 
- 
-  <slot>
-  </slot>
+		<div class="slot-content">
+			<slot>
+			</slot>
+		</div>
 
-   </div>
+	</div>
 </template>
 
 <script>
@@ -23,6 +24,20 @@ export default {
 </script>
 
 <style>
+
+.child-screen
+{
+	display: flex;
+	height: 100vh;
+	flex-direction: column;
+}
+
+.slot-content
+{
+	flex: 1;
+	overflow: auto;
+}
+
 .icon
 {
 	width: 20px;
@@ -31,8 +46,7 @@ export default {
 
 .tlacitko {
   display: block;
-  flex: 1;
-  height: 40px;
+  flex: 0 0 40px;
   background-color: #1e2d2f;
   color: white;
   text-transform: uppercase;
