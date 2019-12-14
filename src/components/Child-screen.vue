3af5bee8c8da2c88b7224d486ef59e2ecd485244
Tomáš Bončo <tomas.bonco@avast.com>
2019-12-14 20:50:08 +0100

    dokoncili jsme vysledky vyhledavani

--- src/components/Child-screen.vue
+++ src/components/Child-screen.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <router-link to="/search" class="tlacitko">Zpět</router-link>
+
+    <router-link to="/search" class="tlacitko">
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon">
+			<path d="M7 13.1l8.9 8.9c.8-.8.8-2 0-2.8l-6.1-6.1 6-6.1c.8-.8.8-2 0-2.8L7 13.1z"/>
+		</svg>
+		Zpět
+	</router-link>
 
  
   <slot>
@@ -17,6 +23,12 @@ export default {
 </script>
 
 <style>
+.icon
+{
+	width: 20px;
+	fill: white;
+}
+
 .tlacitko {
   display: block;
   flex: 1;
@@ -26,7 +38,6 @@ export default {
   text-transform: uppercase;
   text-decoration: none;
   padding: 20px;
-  width: 100%;
 }
 
 </style>
